6a7f8f69f0700c740d3c943f3b816c28d273b9f6
Aman Ahamed <amanahamed04gmail.com>
2025-07-19 18:42:55 +0530

    Quick Notes - title and ui changes

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -23,8 +23,12 @@ const NoteCard = ({ note, onEdit, onDelete }) => {
 
   return (
     <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.4, ease: "easeOut" }}
       whileHover={{ scale: 1.03 }}
-      className="text-black border border-indigo-500 rounded-2xl p-6 relative  hover:shadow-[0_0_25px_rgba(99,102,241,0.8)] transition-all duration-300"
+      className="text-black border border-indigo-500 rounded-2xl p-6 relative hover:shadow-[0_0_25px_rgba(99,102,241,0.8)] transition-all duration-300"
     >
       {/* Title */}
       <h3 className="font-bold text-xl text-center text-indigo-400 mb-4">
@@ -32,8 +36,27 @@ const NoteCard = ({ note, onEdit, onDelete }) => {
       </h3>
 
       {/* Description */}
-      <div className=" text-justify whitespace-pre-line leading-relaxed mb-3">
-        <ReactMarkdown>
+      <div className="text-justify whitespace-pre-line leading-relaxed mb-3">
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-2xl font-bold text-indigo-600 mb-2">
+                {children}
+              </h1>
+            ),
+            h2: ({ children }) => (
+              <h2 className="text-xl font-semibold text-indigo-500 mb-2">
+                {children}
+              </h2>
+            ),
+            h3: ({ children }) => (
+              <h3 className="text-lg font-medium text-indigo-400 mb-2">
+                {children}
+              </h3>
+            ),
+            p: ({ children }) => <p className="mb-2">{children}</p>,
+          }}
+        >
           {showFullText ? note.description : truncate(note.description)}
         </ReactMarkdown>
         {note.description.length > 200 && (
